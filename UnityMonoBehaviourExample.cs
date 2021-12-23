4ee0ad31ad6170b43e74676ebd373ad86dba5a5b
TNTY100 <46330750+TNTY100@users.noreply.github.com>
2021-12-23 14:21:41 -0500

    Create UnityMonoBehaviourExample.cs

---
+++ UnityMonoBehaviourExample.cs
@@ -0,0 +1,16 @@
+// Also see : https://docs.unity3d.com/ScriptReference/MonoBehaviour.html 
+
+using UnityEngine;
+
+public class SampleMonoBehaviour : MonoBehaviour
+{
+  private void Awake() { }
+  
+  private void OnEnable() { }
+  
+  private void Start() { }
+  
+  private void Update() { }
+  
+  private void OnDisable() { }
+}
