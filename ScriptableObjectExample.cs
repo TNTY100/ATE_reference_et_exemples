59affc472904ba9bf32f2c5dbed56c2c4d7a634c
TNTY100 <46330750+TNTY100@users.noreply.github.com>
2021-12-23 14:27:07 -0500

    Create ScriptableObjectExample.cs

---
+++ ScriptableObjectExample.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Sample")]
+public class ScriptableObjectExample : ScriptableObject
+{
+  privte void Awake() {}
+  
+  privte void OneEnable() {}
+  
+  privte void OnDisable() {}
+  
+  privte void OnDestroy() {}
+}
